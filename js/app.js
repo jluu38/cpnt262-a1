df8da86e5cf0afd8c5a4eae77b111fe643644494
Jess Luu <jessica.luu@edu.sait.ca>
2021-02-19 16:56:27 -0700

    js working w/ two functions and one eventlistener

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
 "use strict";
 
+  function ballToDog() {
+  let ball = document.getElementById("ball");
+  ball.classList.toggle("ball");
+  ball.classList.toggle("toDog");
+};
+
+function tossToDog() {
+  let toss = document.getElementById("throw");
+  let hold = document.getElementById("hold");
+  hold.classList.toggle("hidden");
+  toss.classList.toggle("hidden");
+};
+
+document.addEventListener("click", () => {
+  ballToDog();
+  tossToDog();
+});
