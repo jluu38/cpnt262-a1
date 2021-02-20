df8f3ff47732bc3c844b2db89800d36dc746d630
Jess Luu <jessica.luu@edu.sait.ca>
2021-02-19 18:32:57 -0700

    button text now changes and cosmetic additions

--- js/app.js
+++ js/app.js
@@ -6,14 +6,27 @@
   ball.classList.toggle("toDog");
 };
 
-function tossToDog() {
+function handToggle() {
   let toss = document.getElementById("throw");
   let hold = document.getElementById("hold");
   hold.classList.toggle("hidden");
   toss.classList.toggle("hidden");
+  // document.querySelector("button").innerText = "Catch";
+  // decided against the above so as to not clutter with more classes
 };
 
-document.addEventListener("click", () => {
+function buttonTextSwap() {
+const button = document.getElementById("button");
+  if (button.getAttribute("data-text-swap") == button.innerHTML) {
+    button.innerHTML = button.getAttribute("data-text-original");
+  } else {
+    button.setAttribute("data-text-original", button.innerHTML);
+    button.innerHTML = button.getAttribute("data-text-swap");
+  }
+}
+
+button.addEventListener("click", () => {
   ballToDog();
-  tossToDog();
+  handToggle();
+  buttonTextSwap();
 });
